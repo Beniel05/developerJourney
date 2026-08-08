03e4208536f62094147204f476c04c29dcaf88fa
beniel05 <benielebenezer@gmail.com>
2026-08-08 17:24:32 +0530

    Used JS DOM to render elements - Pokemon Images with #spans

---
+++ Frontend/JS/12_DOM_Intro_CODE/Pokemon/app.js
@@ -0,0 +1,23 @@
+// First img URL: https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+// 1 - 1025 images.
+
+const container = document.querySelector('#container');
+const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+
+
+for (let i = 1; i <= 10; i++) {
+
+    const pokemon = document.createElement('div');
+    pokemon.classList.add('pokemon');
+
+    const label = document.createElement('span');
+    label.innerText = `#${i}`;
+
+    const newImg = document.createElement('img');
+    newImg.src = `${baseURL}${i}.png`
+
+    pokemon.appendChild(newImg);
+    pokemon.appendChild(label);
+
+    container.appendChild(pokemon)
+}
