04a0b745453398eb3a526d35c7b2fee8a623a083
beniel05 <benielebenezer@gmail.com>
2026-08-12 22:32:30 +0530

    Added font-family in App.css

--- Frontend/REACT/first-react-app/src/App.jsx
+++ Frontend/REACT/first-react-app/src/App.jsx
@@ -1,13 +1,21 @@
-// import "./App.css";
+import "./App.css";
 
 import Cat from "./Cat.jsx";
+import Greeter from "./Greeter.jsx";
+import Die from './Die.jsx'
 
 function App() {
   return (
     <div>
       <Cat />
-      <Cat />
-      <Cat />
+
+      {/* Props */}
+      <Greeter name="Alex" age="20" />
+      <Greeter name="Bob" age="21" />
+    
+      <Die sides={18} />
+      <Die sides={6} />
+      <Die sides={12} />
     </div>
   );
 }
