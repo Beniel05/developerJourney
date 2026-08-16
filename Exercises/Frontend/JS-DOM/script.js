ec57c12277f73099fcbff4d279ec9375c71b4fa5
beniel05 <benielebenezer@gmail.com>
2026-08-16 23:23:07 +0530

    Practiced some challenges on DOM - JS

--- Exercises/Frontend/JS-DOM/script.js
+++ Exercises/Frontend/JS-DOM/script.js
@@ -9,17 +9,76 @@ btn1.addEventListener("click", () => {
 // btn1.onclick = () => { para1.innerText = changeText1 };
 
 // E2
-document.getElementById('highlightFirstCity').addEventListener('click', () => {
-    // The ?. ensures it only runs .classList if an element actually exists
-    document.getElementById('citiesList').firstElementChild.classList?.add('highlight')
-}) 
+document.getElementById("highlightFirstCity").addEventListener("click", () => {
+  // The ?. ensures it only runs .classList if an element actually exists
+  document
+    .getElementById("citiesList")
+    .firstElementChild.classList?.add("highlight");
+});
 
-document.getElementById('highlightFirstCity').addEventListener('click', function () {
+document
+  .getElementById("highlightFirstCity")
+  .addEventListener("click", function () {
     // The ?. ensures it only runs .classList if an element actually exists
-    this.firstElementChild.classList?.add('highlight')
-}) 
+    this.firstElementChild.classList?.add("highlight");
+  });
 
 // E3
-document.getElementById('changeOrder').addEventListener('click', () => {
-    document.getElementById('coffeeType').innerText = "Espresso";
+document.getElementById("changeOrder").addEventListener("click", () => {
+  const text = document.getElementById("coffeeType");
+  text.innerText = "Espresso";
+
+  const textStyle = text.style;
+  textStyle.backgroundColor = "brown";
+  textStyle.padding = "5px";
+});
+
+// E4
+document.getElementById("addNewItem").addEventListener("click", () => {
+  const newItem = document.createElement("li");
+  newItem.innerText = "Egg";
+
+  document.getElementById("shoppingList").appendChild(newItem);
+});
+
+// E5
+document.getElementById("removeLastTask").addEventListener("click", () => {
+  const taskList = document.getElementById("taskList");
+  // taskList.removeChild(taskList.lastElementChild);
+  taskList.lastElementChild.remove();
+});
+
+// E6
+document.getElementById("clickMeButton").addEventListener("dblclick", () => {
+  alert("Got Clicked!");
+});
+
+// E7
+document.getElementById("teaList").addEventListener("click", (e) => {
+  if (e.target && e.target.matches(".teaItem")) {
+    alert("You selected: " + e.target.textContent);
+  }
+});
+
+// E8
+document
+  .getElementById("feedbackForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    let feedback = document.getElementById("feedbackInput").value;
+    console.log(feedback);
+    document.getElementById(
+      "feedbackDisplay"
+    ).textContent = `Feedback is: ${feedback}`;
+  });
+
+// E9
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("domStatus").textContent = "DOM fully loaded";
+});
+
+// E10
+document.getElementById('toggleHighlight').addEventListener('click', () => {
+    document.getElementById('descriptionText').classList.toggle('highlight');
 })
