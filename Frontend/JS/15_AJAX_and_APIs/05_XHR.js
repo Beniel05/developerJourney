595298ed64e361044e7f46aa3c7433efc53af8d1
beniel05 <benielebenezer@gmail.com>
2026-08-23 21:00:59 +0530

    Make request using XMLHtmlRequest()

---
+++ Frontend/JS/15_AJAX_and_APIs/05_XHR.js
@@ -0,0 +1,22 @@
+// XHR (aka) XMLHttpRequest
+
+// https://swapi.dev/api/people/1/
+// https://swapi.tech/api/people/1/
+
+const req = new XMLHttpRequest();
+
+req.onload = function() {
+    console.log("LOADED");
+    const data = JSON.parse(this.response);
+    console.log(data);
+    console.log(`Name: ${data.name}\nGender: ${data.gender}\nHairColor: ${data.hair_color}`);
+}
+
+req.onerror = function() {
+    console.log("ERROR");
+    console.log(this);
+
+}
+
+req.open("GET", "https://swapi.dev/api/people/1/");
+req.send();
