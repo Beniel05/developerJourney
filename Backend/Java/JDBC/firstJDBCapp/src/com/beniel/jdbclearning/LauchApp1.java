59f1998e88abd237ee59bb80793eb1db89cd3bc8
beniel05 <benielebenezer@gmail.com>
2026-09-10 13:10:15 +0530

    Basic JDBC startup steps: Load the Driver - Creating Connection - Creating Statement - (UnImplemented: Executing Query - Process the Result) - Closing all the resources

--- Backend/Java/JDBC/firstJDBCapp/src/com/beniel/jdbclearning/LauchApp1.java
+++ Backend/Java/JDBC/firstJDBCapp/src/com/beniel/jdbclearning/LauchApp1.java
@@ -1,10 +1,29 @@
 package com.beniel.jdbclearning;
+import java.sql.*;
 
 public class LauchApp1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
+		// Load and Register the Driver
+		Class.forName("com.mysql.cj.jdbc.Driver");
+	
+		// Establish the connection
+		String url = "jdbc:mysql://localhost:3306/jdbclearning";
+		String user = "root";
+		String password = "root";
+		Connection connect = DriverManager.getConnection(url, user, password);
+		
+		// Creating statement
+		Statement statement = connect.createStatement();
+		
+		// Execute the query
+		
+		// Process the response
+		
+		// Close all the resources
+		statement.close();
+		connect.close();
 	}
 
 }
