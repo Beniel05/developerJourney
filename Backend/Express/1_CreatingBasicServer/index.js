e02e0a522a07899381df04cbf1f41612fc5c3403
beniel05 <benielebenezer@gmail.com>
2026-09-13 17:02:08 +0530

    Practice GET method - and single POST method to know both are different methods and the path result will varies based on the Request type : GET, POST, PUT, DELETE and PATCH

--- Backend/Express/1_CreatingBasicServer/index.js
+++ Backend/Express/1_CreatingBasicServer/index.js
@@ -3,16 +3,40 @@ const express = require("express");
 const app = express();
 // console.dir(app);
 
-app.use((req, res) => {
-    /* app.use() is an Express method used to mount middleware functions that run for 
-    every incoming request matching a specified URL path prefix, regardless of the HTTP method. */
-    console.log(`Server got visited. Path: ${req.path}`);
-    res.send(`<h3>We got request from Path: ${req.path}</h3>`)
+/* Standard HTTP specification, a server can only send one HTTP response for one HTTP request */
+// app.use((req, res) => {
+//     /* app.use() is an Express method used to mount middleware functions that run for 
+//     every incoming request matching a specified URL path prefix, regardless of the HTTP method. */
+//     console.log(`Server got visited. Path: ${req.path}`);
+//     res.send(`<h3>We got request from Path: ${req.path}</h3>`)
+// })
+
+app.get('/', (req, res) => {
+    console.log(`${req.path} visited.`);
+    res.send("HOME Page!")
 })
 
-// app.get('/', (req, res) => {
-//     res.send("Welcome to the server.")
-// })
+app.get('/cats', (req, res) => {
+    console.log(`${req.url} visited.`);
+    res.send("Meow!")
+})
+
+app.get('/dogs', (req, res) => {
+    console.log(`${req.url} visited.`);
+    res.send("Bark!")
+})
+
+app.post('/dogs', (req, res) => {
+    console.log(req.url);
+    res.send("POST Request from /dogs !!")
+})
+
+// This '*path' should be at the last.
+// This will take care of all other possible GET /paths which are not written by us in the top.
+// If this placed at very beginning all the GET /paths - even written ones - will be ignored by this.
+app.get("*path", (req, res) => {
+    res.send("I don't know what you're trying to access.")
+})
 
 const PORT = 8080;
 app.listen(PORT, () => {
