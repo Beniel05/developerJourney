2faf2a6eaef6d6dee030b229663a748bbebec99f
beniel05 <benielebenezer@gmail.com>
2026-09-17 14:55:04 +0530

    Understood the flow of useState and re-rendering

--- Frontend/REACT/1_CrashCourse/3-BasicState/src/App.jsx
+++ Frontend/REACT/1_CrashCourse/3-BasicState/src/App.jsx
@@ -3,13 +3,15 @@ import './App.css'
 import Counter from './components/Counter.jsx';
 import Toggler from './components/Toggler.jsx';
 import TwoStatesInOneComp from './components/TwoStatesInOneComp.jsx';
+import UseStateRenderDemo from './components/UseStateRenderDemo.jsx';
 
 function App() {
   return (
     <div>
       {/* <Counter />
       <Toggler /> */}
-      <TwoStatesInOneComp />
+      {/* <TwoStatesInOneComp /> */}
+      <UseStateRenderDemo />
     </div>
   )
 }
